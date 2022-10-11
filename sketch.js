2b60b2e2c3ee52a577f7901c2638b65d3a4dc2b3
marcianito-project <115181121+marcianito-project@users.noreply.github.com>
2022-10-11 23:52:31 +0200

    segundo

--- sketch.js
+++ sketch.js
@@ -1,22 +1,56 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(220)
+var img;
+var canvasSize=500;
+var pg;
+
+function preload(){
+  img = loadImage('sunimage.jpg');
 }
+
+function setup() {
+  createCanvas(canvasSize, canvasSize+100);
+  textSize(32);
+  background(220);
+  pg = createGraphics(canvasSize, canvasSize);
+
+}
+
+
+ 
+function mousePressed(){
+  strokeWeight(10);
+  pg.fill(255, 0, 0);
+  pg.ellipse(mouseX, mouseY, 20, 20);
+  //fill(255,255,0,255);
+  //let Xc=mouseX;
+  //let Xy=mouseY;
+  //circle(Xc,Xy,10)
+  
+}
+
+
+
 
 function draw() {
-    fill(random(255),random(255),random(255))
-    circle(random(windowWidth),random(windowHeight),random(200))
+//Carga imagen
+  img.resize(canvasSize, canvasSize);
+  image(img,0,0);
+  
+//Dibujo del circulo de lectura
+  strokeWeight(2);
+  stroke(255);
+  fill(0,0,0,0);
+   
+  let x=mouseX-250;
+  let y=mouseY-250;
+  let r=abs(sqrt(pow(x,2)+pow(y,2)));
+  let d = 2*r;
+  if (d<canvasSize){
+  circle(250,250,d);    
+  } else {
+  circle(250,250,canvasSize);  
+  }
 
-}
-
-function mousePressed() {
-    userStartAudio();
-    for (var i=0; i<100; i++) {
-        fill(random(255),random(255),random(255))
-        circle(random(800),random(800),random(200))
-    }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+  image(pg,0,0);
+  
+  
+} 
